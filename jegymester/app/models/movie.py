5409f15017636b96ae6fccd5da42f5d08171a971
punty <punty@LAPTOP-IRA6KVMI>
2026-03-03 18:25:11 +0100

    Add project files.

---
+++ jegymester/app/models/movie.py
@@ -0,0 +1,16 @@
+﻿from __future__ import annotations
+from app.extensions import db
+from typing import List
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.types import String, Integer
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    duration: Mapped[int] = mapped_column(Integer, nullable=False)
+    genre: Mapped[str] = mapped_column(String(50), nullable=False)
+    age_limit: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+
+    screenings: Mapped[List["Screening"]] = relationship(back_populates="movie")
